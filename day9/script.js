cbc158dcf88fa6835d8cd8dafbda03aee732f1e8
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2015-06-19 13:54:16 +0530

    initial js stuff

---
+++ day9/script.js
@@ -0,0 +1,15 @@
+/*for(var x=1;x<=100;x++){
+	if(x%2==0){
+		console.log(x);
+	}
+}
+var x=prompt('Enter a  number?');
+if(x>100){
+	console.log('number is greater than 100');
+}else{
+	console.log('no less than 100');
+}*/
+var radius=prompt('Enter radius?');
+var area=3.14*radius*radius;
+console.log('the radius entered was :: '+radius);
+console.log('its calculated area :: '+area);
